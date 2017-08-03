1a8eef27b770f6925f7091287759436c6472dd61
admin-m <977986310@qq.com>
2017-08-03 17:17:55 +0800

    完成结算功能

--- public/index.js
+++ public/index.js
@@ -10,7 +10,7 @@ $(function () {
             <td>${item.price}元...</td>
             <td>数量:</td>
             <td>
-                <input type="number" name="number" id="itemNumber" style="width: 50px">
+                <input  name="number" id="itemNumber" class="itemNumber" style="width: 50px" onkeyup="if(this.value.length==1){this.value=this.value.replace(/[^1-9]/g,'')}else{this.value=this.value.replace(/\\D/g,'')}" >
             </td>
         </tr>
         </tr><br><br>`)
@@ -20,12 +20,12 @@ $(function () {
   for (let item of loadPromotions()) {
     let items = item.items;
     let itemsInfo = [];
-    if (items === undefined){
+    if (items === undefined) {
       itemsInfo = "";
-    }else {
-      for (let barCode of items){
-        for (let elem of loadAllItems()){
-          if (elem.id === barCode){
+    } else {
+      for (let barCode of items) {
+        for (let elem of loadAllItems()) {
+          if (elem.id === barCode) {
             itemsInfo.push(elem.name)
           }
         }
@@ -40,11 +40,24 @@ $(function () {
         </tr><br><br>`)
   }
 })
-
+function keyPress(ob) {
+  if (!ob.value.match(/^[\+\-]?\d*?\.?\d*?$/)) ob.value = ob.t_value; else ob.t_value = ob.value; if (ob.value.match(/^(?:[\+\-]?\d+(?:\.\d+)?)?$/)) ob.o_value = ob.value;
+}
 
 function calculatePrice() {
   // 想办法调用`bestCharge`并且把返回的字符串
   // 显示在html页面的`message`中
+  let itemNumber = $(".itemNumber");
+  let items = [];
+  for (let i = 0; i < loadAllItems().length; i++) {
+    let item = "";
+    item += loadAllItems()[i].id;
+    item += " x ";
+    item += $(itemNumber[i]).val();
+    items.push(item);
+  }
+
+  $("#demo").text(autoChooseBestCharge(items));
 }
 
 function clear() {
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,67 +1,34 @@
+
+function autoChooseBestCharge(selectedItems) {
+
+  return comparisonScheme(mergeItems(splitJoint(selectedItems)))
+}
+
+function comparisonScheme(items) {
+  let a1 = Number(specifyDiscount_HalfPrice(items)[1]);
+  let a2 = Number(specifyDiscount_Over30Minus6(items)[1]);
+
+  if (a1<a2) {
+
+    return specifyDiscount_HalfPrice(items)[0];
+  } else if (a2<a1) {
+
+    return specifyDiscount_Over30Minus6(items)[0];
+  }
+}
+
 function bestCharge(selectedItems) {
 
   if (selectedItems.length === 3) {
-    let html_head = `============= 订餐明细 =============\n`;
-    let html_middle = `-----------------------------------\n`;
-    let html_tail = `===================================\n`;
-    let html_item = "";
 
-    let save = 0;
-    let all = 0;
-
-    for (let item of mergeItems(splitJoint(selectedItems))) {
-      if (item.name === "黄焖鸡" || item.name === "凉皮") {
-        var itemPrice = (item.price / 2) * item.count;
-        save += (item.price / 2) * item.count;
-      } else {
-        var itemPrice = item.price * item.count;
-      }
-      all += itemPrice;
-      html_item += `${item.name} x ${item.count} = ${item.price * item.count}元\n`
-    }
-    let html_saveInfo = `使用优惠:\n指定菜品半价(黄焖鸡，凉皮)，省${save}元\n`;
-    let html_allInfo = `总计：${all}元\n`;
-
-    return html_head + html_item + html_middle + html_saveInfo + html_middle + html_allInfo + html_tail;
+    return specifyDiscount_HalfPrice(mergeItems(splitJoint(selectedItems)))[0]
   } else if (selectedItems.length === 2) {
-    let html_head = `============= 订餐明细 =============\n`;
-    let html_middle = `-----------------------------------\n`;
-    let html_tail = `===================================\n`;
-    let html_item = "";
 
-    let save = 0;
-    let all = 0;
-
-    for (let item of mergeItems(splitJoint(selectedItems))) {
-      all += item.price * item.count;
-      html_item += `${item.name} x ${item.count} = ${item.price * item.count}元\n`
-    }
-    if (all > 30) {
-      var html_allInfo = `总计：${all - 6}元\n`;
-    }else {
-      var html_allInfo = `总计：${all}元\n`;
-    }
-    let html_saveInfo = `使用优惠:\n满30减6元，省6元\n`;
-
-    return html_head + html_item + html_middle + html_saveInfo + html_middle + html_allInfo + html_tail;
+    return specifyDiscount_Over30Minus6(mergeItems(splitJoint(selectedItems)))[0]
   } else if (selectedItems.length === 1){
-    let html_head = `============= 订餐明细 =============\n`;
-    let html_middle = `-----------------------------------\n`;
-    let html_tail = `===================================\n`;
-    let html_item = "";
 
-    let all = 0;
-
-    for (let item of mergeItems(splitJoint(selectedItems))) {
-      all += item.price * item.count;
-      html_item += `${item.name} x ${item.count} = ${item.price * item.count}元\n`
-    }
-    let html_allInfo = `总计：${all}元\n`;
-
-    return html_head + html_item + html_middle + html_allInfo + html_tail;
+    return noSpecifyDiscount(mergeItems(splitJoint(selectedItems)))[0]
   }
-
-
 }
 
 function splitJoint(selectedItems) {
@@ -86,4 +53,81 @@ function mergeItems(splitJoint) {
   }
 
   return mergeItemInfo;
+}
+
+function specifyDiscount_HalfPrice(mergeItems) {
+  let html_head = `============= 订餐明细 =============\n`;
+  let html_middle = `-----------------------------------\n`;
+  let html_tail = `===================================\n`;
+  let html_item = "";
+
+  let save = 0;
+  let all = 0;
+
+  for (let item of mergeItems) {
+    if (item.name === "黄焖鸡" || item.name === "凉皮") {
+      var itemPrice = (item.price / 2) * item.count;
+      save += (item.price / 2) * item.count;
+    } else {
+      var itemPrice = item.price * item.count;
+    }
+    all += itemPrice;
+    html_item += `${item.name} x ${item.count} = ${item.price * item.count}元\n`
+  }
+  let html_saveInfo = `使用优惠:\n指定菜品半价(黄焖鸡，凉皮)，省${save}元\n`;
+  let html_allInfo = `总计：${all}元\n`;
+
+  let itemInfosAndPrice = [];
+  let itemInfos = html_head + html_item + html_middle + html_saveInfo + html_middle + html_allInfo + html_tail;
+  itemInfosAndPrice.push(itemInfos,all);
+
+  return itemInfosAndPrice;
+}
+
+function specifyDiscount_Over30Minus6(mergeItems) {
+  let html_head = `============= 订餐明细 =============\n`;
+  let html_middle = `-----------------------------------\n`;
+  let html_tail = `===================================\n`;
+  let html_item = "";
+
+  let save = 0;
+  let all = 0;
+
+  for (let item of mergeItems) {
+    all += item.price * item.count;
+    html_item += `${item.name} x ${item.count} = ${item.price * item.count}元\n`
+  }
+  if (all > 30) {
+    var html_allInfo = `总计：${all - 6}元\n`;
+  }else {
+    var html_allInfo = `总计：${all}元\n`;
+  }
+  let html_saveInfo = `使用优惠:\n满30减6元，省6元\n`;
+
+  let itemInfosAndPrice = [];
+  let itemInfos = html_head + html_item + html_middle + html_saveInfo + html_middle + html_allInfo + html_tail;
+  itemInfosAndPrice.push(itemInfos,all);
+
+  return itemInfosAndPrice;
+}
+
+function noSpecifyDiscount(mergeItems) {
+  let html_head = `============= 订餐明细 =============\n`;
+  let html_middle = `-----------------------------------\n`;
+  let html_tail = `===================================\n`;
+  let html_item = "";
+
+  let all = 0;
+
+  for (let item of mergeItems) {
+    all += item.price * item.count;
+    html_item += `${item.name} x ${item.count} = ${item.price * item.count}元\n`
+  }
+  let html_allInfo = `总计：${all}元\n`;
+
+  let itemInfosAndPrice = [];
+  let itemInfos = html_head + html_item + html_middle + html_allInfo + html_tail;
+  itemInfosAndPrice.push(itemInfos,all);
+
+  return itemInfosAndPrice;
 }
